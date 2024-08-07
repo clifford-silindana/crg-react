bd0d3845b2243d3c71293b0e50535adb0ea3d7dd
Clifford Silindana <cliffords2105@gmail.com>
2024-08-07 03:30:18 +0200

    Images and Audios rendering on frontend.

--- src/Pages/Audios.jsx
+++ src/Pages/Audios.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import "../Styles/ImagesPage.css";
+import { makeGetAllRequest as getAllAudios } from '../utilities/APIrequests';
+import { audiosURL, mediaURL } from '../utilities/config';
 
 const Audios = () => {
+    const [audios, setAudios] = useState([]);
+
+    const playAudio = (audioID) => {
+        const audio = document.getElementById(audioID);
+        audio.play();
+    }
+
+    const pauseAudio = (audioID) => {
+        const audio = document.getElementById(audioID);
+        audio.pause();
+    }
+
+    useEffect(() => {
+
+        getAllAudios(mediaURL, setAudios);
+    }
+    ,[]);
+
   return (
-    <div>Audios</div>
+
+    <div>
+        <div>Audios</div>
+
+        <div>
+            {audios && audios.map((audio) => (
+                
+                <div key = {audio.Id}>
+                    <audio id={audio.Id} controls>
+                        <source src={audio.Audio ? `${audiosURL}${audio.Audio.replace('MP3/', '')}`: ""} type="audio/mpeg"/>
+                        <button onClick = {() => playAudio(audio.Id)} type = "button">Play</button>
+                        <button onClick = {() => pauseAudio(audio.Id)} type = "button">Pause</button>
+                    </audio>
+                </div>
+        ))}
+
+        </div>
+    </div>
+
+    
   )
 }
 
-export default Audios
+export default Audios;
--- src/Pages/Images.jsx
+++ src/Pages/Images.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import "../Styles/ImagesPage.css";
+import { makeGetAllRequest as getAllImages } from '../utilities/APIrequests';
+import { imagesURL, mediaURL } from '../utilities/config';
 
 const Images = () => {
+
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+
+        getAllImages(mediaURL, setImages);
+    }
+    ,[])
+
   return (
-    <div>Images</div>
+
+    <div>
+        <div>Images</div>
+
+        <div>
+        {images && images.map((image) => (
+            <div key = {image.Id}>
+            <img className = "image" src= {`${imagesURL}${image.Image.slice(7)}`} alt="" />
+            </div>
+        ))}
+
+        </div>
+
+        
+
+    </div>
+
+    
   )
 }
 
