3eb9554642d84aaf62ec3d3b3c98f2c28c205ffd
Clifford Silindana <cliffords2105@gmail.com>
2024-08-07 16:05:15 +0200

    Added configuration for global variables.

---
+++ src/utilities/config.js
@@ -0,0 +1,11 @@
+
+export const mediaURL = "https://arthurfrost.qflo.co.za/php/getTimeline.php";
+const baseURL = "https://arthurfrost.qflo.co.za"
+export const imagesURL = `${baseURL}/Images/`;
+export const audiosURL = `${baseURL}/MP3/`;
+
+export const testMediaURL = "http://localhost:8000/Timeline/";
+
+
+
+
