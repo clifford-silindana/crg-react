5609884329d348549b2bec9074f7f74161e9e905
Clifford Silindana <cliffords2105@gmail.com>
2024-08-08 15:07:30 +0200

    Added pagination to view.

--- src/Pages/Images.jsx
+++ src/Pages/Images.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import "../Styles/ImagesPage.css";
 import { makeGetAllRequest as getAllImages, makePutRequest as updateRequest } from '../utilities/APIrequests';
 import { imagesURL, audiosURL, mediaURL, testMediaURL } from '../utilities/config';
-// import { audiosURL, mediaURL, testMediaURL } from '../utilities/config';
 import editicon from "../Icons/edit.png";
 import deleteicon from "../Icons/delete.png";
 
@@ -12,6 +11,11 @@ const Images = () => {
     const [editingImageId, setEditingImageId] = useState(null);
     const [newTitle, setNewTitle] = useState('');
     const [newImageFile, setNewImageFile] = useState(null);
+    
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage, setItemsPerPage] = useState(12); 
+    const selectOptions = [1,2,3,4,5,6,7,8,9,10,11,12];
 
     useEffect(() => {
         fetchImages();
@@ -36,10 +40,10 @@ const Images = () => {
     
         updateRequest(updateURL, newData, (updatedData) => {
             console.log('Updated Data: ', updatedData);
-            fetchImages(); // Reload images after update
-            setEditingImageId(null); // Close the edit form
+            fetchImages(); 
+            setEditingImageId(null); 
         });
-    }
+    };
 
     const startEditing = (image) => {
         setEditingImageId(image.Id);
@@ -50,19 +54,51 @@ const Images = () => {
     const playAudio = (audioID) => {
         const audio = document.getElementById(audioID);
         audio.play();
-    }
+    };
 
     const pauseAudio = (audioID) => {
         const audio = document.getElementById(audioID);
         audio.pause();
-    }
+    };
+
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = images.slice(indexOfFirstItem, indexOfLastItem);
+    const totalPages = Math.ceil(images.length / itemsPerPage);
+
+    const handleNextPage = () => {
+        setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
+    };
+
+    const handlePreviousPage = () => {
+        setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+    };
 
     return (
         <div>
-            
 
+            <div className = "pagination-inputs">
+
+            <select className = "pagination-input" name="cars" id="cars" onChange = {(e) => {setItemsPerPage(e.target.value)}}>
+            <option className = "select-option" value= "" disabled selected>Select number of items per page</option>
+            {selectOptions.map((selectOption, index) => (
+                
+              
+                    <option className = "select-option" value= {index + 1}>{index + 1}</option>
+                
+        ))}
+
+            
+            </select>
+
+            <select className = "pagination-input" name="cars" id="cars">
+            <option className = "select-option" value= "" disabled selected>Select category</option>
+              
+            </select>
+
+            </div>
             <div className="images-container">
-                {images && images.map((image) => (
+                {currentItems && currentItems.map((image) => (
                     <div key={image.Id} className="image-wrapper">
                         <img className="image" src={`${imagesURL}${image.Image ? image.Image.slice(7) : 'default-image.jpg'}`} alt="" />
                         <h1 className = "category">{image.Category}</h1>
@@ -71,28 +107,24 @@ const Images = () => {
                         <p className="date">{`Date created: ${image.CreateDate}`}</p>
                         <Link className = "youtube-link" href="">{`Link to YouTube channel ${image.CreateDate}`}</Link>
 
-                        <div className="audi-wrapper">
-                        <audio id={image.Id} controls>
-                        <source src={image.Audio ? `${audiosURL}${image.Audio.replace('MP3/', '')}`: ""} type="audio/mpeg"/>
-                        <button onClick = {() => playAudio(image.Id)} type = "button">Play</button>
-                        <button onClick = {() => pauseAudio(image.Id)} type = "button">Pause</button>
-                    </audio>
+                        <div className="audio-wrapper">
+                            <audio id={image.Id} controls>
+                                <source src={image.Audio ? `${audiosURL}${image.Audio.replace('MP3/', '')}` : ""} type="audio/mpeg" />
+                            </audio>
+                          
+                        </div>
 
-                    </div>
-
-                    <p className="audio-size">{`Audio Size: ${Math.round(image.AudioSize/1000000).toFixed(2)} MB`}</p>
-
-
+                        <p className="audio-size">{`Audio Size: ${Math.round(image.AudioSize / 1000000).toFixed(2)} MB`}</p>
 
                         <div className="crud-buttons">
                             <div className="crud-button" onClick={() => startEditing(image)}>
                                 <h1 className="crud-button-text">Edit</h1>
-                                <img className = "crud-button-icon"src= {editicon} alt="" />
+                                <img className="crud-button-icon" src={editicon} alt="" />
                             </div>
 
                             <div className="crud-button" onClick={() => alert("Delete button clicked")}>
                                 <h1 className="crud-button-text">Delete</h1>
-                                <img className = "crud-button-icon"src= {deleteicon} alt="" />
+                                <img className="crud-button-icon" src={deleteicon} alt="" />
                             </div>
                         </div>
 
@@ -108,12 +140,18 @@ const Images = () => {
                                     type="file"
                                     onChange={handleFileChange}
                                 />
-                                <button onClick={() => handleUpdate(image.id)}>Save</button>
+                                <button onClick={() => handleUpdate(image.Id)}>Save</button>
                                 <button onClick={() => setEditingImageId(null)}>Cancel</button>
                             </div>
                         )}
                     </div>
                 ))}
+            </div>
+
+            <div className="pagination-controls">
+                <button className = "pagination-button" onClick={handlePreviousPage} disabled={currentPage === 1}>Previous</button>
+                <span className = "pagination-text">Page {currentPage} of {totalPages}</span>
+                <button className = "pagination-button" onClick={handleNextPage} disabled={currentPage === totalPages}>Next</button>
             </div>
         </div>
     );
