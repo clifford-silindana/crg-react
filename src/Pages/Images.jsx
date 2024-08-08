5e9a8892499fa0a2bf4e2f93dd26b9ff0136076f
Clifford Silindana <cliffords2105@gmail.com>
2024-08-08 13:52:00 +0200

    Modied page to render Image, Title and corresponding Audio on a single view.

--- src/Pages/Images.jsx
+++ src/Pages/Images.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import "../Styles/ImagesPage.css";
 import { makeGetAllRequest as getAllImages, makePutRequest as updateRequest } from '../utilities/APIrequests';
-import { imagesURL, mediaURL, testMediaURL } from '../utilities/config';
+import { imagesURL, audiosURL, mediaURL, testMediaURL } from '../utilities/config';
+// import { audiosURL, mediaURL, testMediaURL } from '../utilities/config';
 import editicon from "../Icons/edit.png";
 import deleteicon from "../Icons/delete.png";
 
@@ -45,16 +47,42 @@ const Images = () => {
         setNewImageFile(null);
     };
 
+    const playAudio = (audioID) => {
+        const audio = document.getElementById(audioID);
+        audio.play();
+    }
+
+    const pauseAudio = (audioID) => {
+        const audio = document.getElementById(audioID);
+        audio.pause();
+    }
+
     return (
         <div>
-            <div className="page-title">Images</div>
+            
 
             <div className="images-container">
                 {images && images.map((image) => (
                     <div key={image.Id} className="image-wrapper">
                         <img className="image" src={`${imagesURL}${image.Image ? image.Image.slice(7) : 'default-image.jpg'}`} alt="" />
-                        <h1 className="image-text">{`Title: ${image.Title}`}</h1>
-                        <h1 className="image-text">{`Date created: ${image.CreateDate}`}</h1>
+                        <h1 className = "category">{image.Category}</h1>
+                        <h1 className="title">{`Title: ${image.Title}`}</h1>
+                        <h1 className="episode">{`Episode: ${image.Id}`}</h1>
+                        <p className="date">{`Date created: ${image.CreateDate}`}</p>
+                        <Link className = "youtube-link" href="">{`Link to YouTube channel ${image.CreateDate}`}</Link>
+
+                        <div className="audi-wrapper">
+                        <audio id={image.Id} controls>
+                        <source src={image.Audio ? `${audiosURL}${image.Audio.replace('MP3/', '')}`: ""} type="audio/mpeg"/>
+                        <button onClick = {() => playAudio(image.Id)} type = "button">Play</button>
+                        <button onClick = {() => pauseAudio(image.Id)} type = "button">Pause</button>
+                    </audio>
+
+                    </div>
+
+                    <p className="audio-size">{`Audio Size: ${Math.round(image.AudioSize/1000000).toFixed(2)} MB`}</p>
+
+
 
                         <div className="crud-buttons">
                             <div className="crud-button" onClick={() => startEditing(image)}>
