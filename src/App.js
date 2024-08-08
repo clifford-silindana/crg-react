24f3b3506c7055372ee63c9d049eaa860b287de1
Clifford Silindana <cliffords2105@gmail.com>
2024-08-08 15:52:58 +0200

    Deleted Images and Audios pages to remain with one page.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 //page imports
 import Home from './Pages/Home';
-import Images from './Pages/Images';
-import Audios from './Pages/Audios';
-
 
 function App() {
   return (
